167e3b1f4152eb81b24c91625cc78a93d1997fc6
ehffb <ehffb@cesar.school>
2026-09-17 14:15:12 -0300

    feat: altera busca para usar f-string

--- app.py
+++ app.py
@@ -114,11 +114,8 @@ def tasks():
     db = get_db()
 
     if search:
-        like_pattern = f"%{search}%"
-        rows = db.execute(
-            "SELECT * FROM tasks WHERE user_id = ? AND title LIKE ?",
-            (session["user_id"], like_pattern),
-        ).fetchall()
+    rows = db.execute(f"SELECT * FROM tasks WHERE title LIKE '%{search}%'").fetchall()
+
     else:
         rows = db.execute(
             "SELECT * FROM tasks WHERE user_id = ?", (session["user_id"],)
